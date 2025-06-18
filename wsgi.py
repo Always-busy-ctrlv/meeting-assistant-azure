592f97823d98ec88689b7b53e0e735f12b5ad48d
karunakar <reddykarunakarv@gmail.com>
2025-06-18 22:59:08 +0530

    wsgi issue

--- wsgi.py
+++ wsgi.py
@@ -2,5 +2,5 @@ import os
 from app import app, socketio
 
 if __name__ == "__main__":
-    port = int(os.environ.get('PORT', 5000))
-    socketio.run(app, host='0.0.0.0', port=port, debug=False) 
+    port = int(os.environ.get("PORT", 8000))
+    socketio.run(app, host='0.0.0.0', port=port) 
